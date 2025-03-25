ab64cf3a4224b6421cebbaf792895166bac3aef9
Han ne <hanluu062019@gmail.com>
2025-03-25 02:52:30 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './src/**/*.{js,jsx,ts,tsx}', // For Create React App
+      './pages/**/*.{js,ts,jsx,tsx}', // For Next.js pages
+      './components/**/*.{js,ts,jsx,tsx}',
+      './app/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: 'class',
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
