e63fe04e008c97608779804e297128fbdd3095be
Han ne <hanluu062019@gmail.com>
2025-04-24 23:48:11 -0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}', // For Create React App
-      './pages/**/*.{js,ts,jsx,tsx}', // For Next.js pages
-      './components/**/*.{js,ts,jsx,tsx}',
-      './app/**/*.{js,ts,jsx,tsx}',
-    ],
-    darkMode: 'class',
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // For Create React App
+    './pages/**/*.{js,ts,jsx,tsx}', // For Next.js pages
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
