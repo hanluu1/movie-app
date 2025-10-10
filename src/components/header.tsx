458122d8c4c9c7095dabce3b64e2fccb2f92aff5
Han ne <hanluu062019@gmail.com>
2025-10-09 23:42:43 -0700

    fix: sing in button when user haven't sign in

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,9 @@
 
 import { useRouter } from 'next/navigation';
 import React from 'react';
+import type { User } from '@supabase/auth-js';
+import { useEffect, useState } from 'react';
+
 import { supabase } from '@/lib/supabaseClient';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -14,12 +17,21 @@ export function Header ({
   showSearch?: () => void;
 }) {
   const router = useRouter();
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.push('/login');
   };
-
+  useEffect(() => {
+    const getUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      setUser(user);
+      setLoading(false);
+    };
+    getUser();
+  }, []);
   return (
     <div className="relative flex items-center justify-between px-6 py-5 bg-gray-900 border-b border-gray-700 shadow-md">
       <Link href='/'>
@@ -36,12 +48,21 @@ export function Header ({
             onClick={showSearch}
           />
         )}
-        <button
-          onClick={handleSignOut}
-          className="z-10 text-gray-300 hover:text-blue-400 transition"
-        >
-        Sign out
-        </button>
+        {user ? (
+          <button
+            onClick={handleSignOut}
+            className="z-10 text-gray-300 hover:text-blue-400 transition"
+          >
+            Sign out
+          </button>
+        ) : (
+          <button
+            onClick={() => router.push('/login')}
+            className="z-10 text-gray-300 hover:text-blue-400 transition"
+          >
+            Sign in
+          </button>
+        )}
       </div>
     </div>
   );
